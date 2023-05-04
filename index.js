83ab07dd2d14c9ce1bbb37c664284b1c95939f44
mosabbir.duet <mosabbir.duet@gmail.com>
2023-05-04 14:49:35 +0600

    Installed cors middleware and added in app for transfer data to client side

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const cors = require('cors')
+const port = process.env.PORT || 5000;
 
 const chefInformation = require('./data/chefInformation.json')
+
+app.use(cors());
 
 app.get('/',(req, res) => {
     res.send('Japanese Chef cuisine app is running.')
 })
 
-app.get('/chef-information', (req, res) => {
+app.get('/chefInformation', (req, res) => {
     res.send(chefInformation)
 })
 
